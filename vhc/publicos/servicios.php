a0225b4a9794ea408de9c0e1669a1f05f608704a
edSantiago01 <santiago7nubesoft@gmail.com>
2016-10-13 01:43:27 -0500

    servicios

---
+++ vhc/publicos/servicios.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="es">
+<!DOCTYPE html>
+<html lang="es">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="author" content="edSantiago" />
+    <meta name="description" content="Ofrecemos una amplia gama de servicios contables, así como servicios de asesoría en Línea.">
+    <meta name="keywords" content="servicios auditoría, servicios asesoría,  asesoría en Línea, servicios contables, servicio jurídico, servicio fiscal, prevención lavado dinero " />
+
+    <link rel="icon" type="../image/png" href="./favicon.png"/>
+
+    <title>  Servicios | Vázquez Hernández Contadores, S. C.</title>
+
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+    <link href="../css/inicio.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+
+    <?php include ("../seccionesVista/barraNavegacionPagina.php"); ?>
+
+
+    <div style="background-image:url('../img/servicios/101371076s.jpg');"class="fondo paddingTexto">
+      <div class="row">
+        <br>
+        <div class="col-md-3 col-md-offset-9">
+          <input type="text" name="name" value="" placeholder="Buscar en el sitio web">
+        </div>
+      </div>
+
+
+      <div class="container">
+
+      <div class="row">
+        <div class="col-lg-12">
+          <br>
+          <h1 class="tituloEntrada tamTitulo grisObscuro"><i class="fa fa-briefcase"></i> Servicios</h1>
+        </div>
+      </div>
+
+      <div class="row">
+      <!-- servicios contables -->
+        <div class="col-md-3 col-sm-6">
+
+          <div class="elemento transladarDerecha">
+          <div class="intro-table intro-table-hover efecto1">
+            <div class="dmask fondoD1">
+    						<div class="bottom">
+
+    						  	<p class="white heading small-pt texto3D tam1"> Contables</p>
+    							  <a href="servicios/contables.php" class="btn btn-md fondoAzulClaro expand"><strong>Ver más</strong></a>
+
+    						</div>
+              </div>
+    				</div>
+          </div>
+
+      </div>
+
+        <div class="col-md-3 col-sm-6">
+          <div class="elemento transladarArriba">
+            <div class="intro-table intro-table-hover efecto2">
+              <div class="dmask fondoD1">
+                  <div class="bottom">
+                      <p class="white heading small-pt texto3D tam1"> Asesorías</p>
+                      <a href="servicios/asesoria.php" class="btn btn-md fondoAzulClaro expand"><strong>Ver más</strong></a>
+                  </div>
+               </div>
+            </div>
+          </div>
+
+          </div>
+
+
+
+          <div class="col-md-3 col-sm-6 ">
+
+            <div class="elemento transladarArriba">
+              <div class="intro-table intro-table-hover efecto3">
+                <div class="dmask fondoD1">
+                    <div class="bottom">
+                        <p class="white heading small-pt texto3D tam1">Auditorías</p>
+                        <a href="servicios/auditoria.php" class="btn btn-md fondoAzulClaro expand"><strong>Ver más</strong></a>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      <!--    </div>
+
+      <div class="row">  -->
+        <!-- Auditoria -->
+
+          <!-- Servicio Juridico -->
+          <div class="col-md-3 col-sm-6">
+            <div class="elemento transladarIzquierda">
+             <div class="intro-table intro-table-hover efecto4">
+                <div class="dmask fondoD1">
+                   <div class="bottom">
+                       <p class="white heading small-pt texto3D tam1">Jurídicos</p>
+                       <a href="servicios/juridico.php" class="btn btn-md fondoAzulClaro expand"><strong>Ver más</strong></a>
+                   </div>
+                 </div>
+              </div>
+            </div>
+            </div>
+
+            <!-- Servicio fiscal -->
+            <div class="col-md-3 col-sm-6">
+              <div class="elemento transladarDerecha">
+
+
+                <div class="intro-table intro-table-hover efecto5">
+                    <div class="dmask fondoD1">
+                      <div class="bottom">
+                          <p class="white heading small-pt texto3D tam1">Fiscal</p>
+                          <a href="servicios/fiscal.php" class="btn btn-md fondoAzulClaro expand"><strong>Ver más</strong></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+
+
+
+              <div class="col-md-3 col-sm-6">
+                <div class="elemento transladarAbajo">
+                  <div class="intro-table intro-table-hover efecto6">
+                      <div class="dmask fondoD1">
+                        <div class="bottom">
+                            <p class="white texto3D tam2 pld">Prevención de Lavado de Dinero</p>
+                            <a href="servicios/pLavado.php" class="btn btn-md fondoAzulClaro expand btnPld"><strong>Ver más</strong></a>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+              </div>
+
+    <!--    </div>
+
+
+      <div class="row"> -->
+        <!-- Servicio Lavado -->
+
+
+        <!-- auditoriaG -->
+    <!--    <div class="col-md-4 col-sm-6">
+
+        <div class="elemento">
+        <div class="intro-table intro-table-hover efecto8">
+
+              <div class="bottom">
+                <div class="dmask fondoD1">
+                  <h3 class="white heading small-pt texto3D"> Auditoría Gubernamental</h3>
+                  <a href="servicios/auditoriaG.php" class="btn btn-lg btn-success expand">Ver más</a>
+                </div>
+              </div>
+            </div>
+      </div>
+      </div> -->
+
+      <div class="col-md-3 col-sm-6"> <!-- Laborales -->
+        <div class="elemento transladarAbajo">
+          <div class="intro-table intro-table-hover efecto7">
+                <!--  <h5 class="white heading hide-hover">Servicios Contables</h5> -->
+                <div class="bottom">
+                  <div class="dmask fondoD1">
+                    <p class="white heading small-pt texto3D tam1"> Laborales</p>
+                    <a href="servicios/laborales.php" class="btn btn-md fondoAzulClaro expand"><strong>Ver más</strong></a>
+                  </div>
+                </div>
+              </div>
+        </div>
+
+      </div>
+
+
+      <div class="col-md-3 col-sm-6">
+        <div class="elemento transladarIzquierda">
+
+          <div class="intro-table intro-table-hover efecto9">
+                <!--  <h5 class="white heading hide-hover">Servicios Contables</h5> -->
+                <div class="bottom">
+                  <div class="dmask fondoD1">
+                    <p class="white heading small-pt texto3D tam1"> Otros</p>
+                    <a href="servicios/otros.php" class="btn btn-md fondoAzulClaro expand"><strong>Ver más</strong></a>
+                  </div>
+                </div>
+              </div>
+
+          </div>
+      </div>
+
+      </div>
+
+      <div class="row">
+
+
+      </div>
+      <!-- Paneles Servicios -->
+
+    </div>
+    </div>
+
+
+
+
+      <?php include ("../seccionesVista/piePagina.php"); ?>
+      <script src="../js/jquery.js"></script>
+      <script src="../js/bootstrap.min.js"></script>
+
+      <script>
+      	$('#navServicios').addClass('activoLi');
+      </script>
+
+    <script charset="utf-8">
+    //jQuery to collapse the navbar on scroll
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+    </script>
+  </body>
+
+</html>
