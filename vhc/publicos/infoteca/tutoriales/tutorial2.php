a1d5d2ed9d23a9dc57534eba21077cddc4eeb215
EdSM <edgar.santiago@contadoresvh.com>
2016-10-13 10:25:08 -0500

    tutorial 1

---
+++ vhc/publicos/infoteca/tutoriales/tutorial2.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <meta name="author" content="edSantiago" />
+  <link rel="icon" type="../../../image/png" href="./favicon.png"/>
+
+  <title>    Tutoriales | Vázquez Hernández Contadores, S. C.
+  </title>
+
+  <link href="../../../css/bootstrap.min.css" rel="stylesheet">
+  <link href="../../../fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+  <link href="../../../css/publicos/pagina.css" rel="stylesheet">
+</head>
+  <body>
+
+      <?php include ("../../../seccionesVista/barraNavegacionPagina.php"); ?>
+
+        <div class="fondoInfoteca fondo paddingMenu">
+            <div class="row">
+              <div class="col-lg-12">
+                <br>
+                <h1 class="tamTitulo grisClaro texto3D tituloEntrada"><i class="fa fa-book"></i> Tutoriales</h1>
+                <br>
+              </div>
+            </div>
+
+            <div class="row">
+
+              <div class="col-md-9 animacionContenido">
+                  <!--CONTENIDO DEL ARTICULO-->
+
+                      <div class="well trasparenteClaroPlus">  <!-- Well -->
+
+                        <video height="400" width="100%" controls>
+                          <source src="{{URL::asset('video/infoteca/tutoriales/formasDeAhorro.mov')}}"  >
+                        </video>
+                        <h3 class="text-center">
+                            Formas de ahorro
+                        </h3>
+
+                      </div> <!-- /well -->
+
+                        <!-- ///*****************************  -->
+
+              </div>
+
+              <div class="col-md-3 transladarIzquierda">
+                   <!--MENU DE LOS ARTICULOS menuArticulo.blde.php -->
+                  <?php include ("../../../seccionesVista/menuTutorial.php"); ?>
+                <br>
+                <a class="btn btn-primary btn-block" href="javascript:history.back()"><i class="fa fa-chevron-left"></i><span class="network-name"> Regresar</span></a>
+              </div>
+
+          </div>
+        </div>
+
+        <?php include ("../../../seccionesVista/piePagina.php"); ?>
+        <script src="../../../js/jquery.js"></script>
+        <script src="../../../js/bootstrap.min.js"></script>
+
+      <script>
+        $('#navInfoteca').addClass('activoLi');
+        //jQuery to collapse the navbar on scroll
+        $(window).scroll(function() {
+            if ($(".navbar").offset().top > 50) {
+                $(".navbar-fixed-top").addClass("top-nav-collapse");
+            } else {
+                $(".navbar-fixed-top").removeClass("top-nav-collapse");
+            }
+        });
+      </script>
+      <script>
+        $('#ulTutorialTem1').addClass('in');
+        $('#liTutorial2').addClass('activoBorde');
+      </script>
+
+
+  </body>
+</html>
