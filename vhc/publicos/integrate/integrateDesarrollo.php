30dee5c3d38942ac261be8eafc0c495526220803
EdSM <edgar.santiago@contadoresvh.com>
2016-10-13 13:00:29 -0500

    tu desarrollo

--- vhc/publicos/integrate/integrateDesarrollo.php
+++ vhc/publicos/integrate/integrateDesarrollo.php
@@ -1,76 +1,153 @@
-@extends('layoutIntegrate')
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <meta name="author" content="edSantiago" />
+  <meta name="description" content="Testimonios y algunos clientes en Vázquez Hernández Contadores, S.C.">
+  <meta name="keywords" content="testimonios, clientes" />
+  <meta name="description" content="Conocer las vacates y podras envíes tu Currículum Vitae así como tus datos curriculares para futuras oportunidades. ">
+  <meta name="keywords" content="postulate, vacantes, desarrollo profesional, desarrollo personal" />
 
-@section('head')
-@stop
 
-@section('title')
-  Tu desarrollo profesional y personal | Vázquez Hernández Contadores, S. C.
-@stop
+  <link rel="icon" type="../image/png" href="./favicon.png"/>
 
-@section('css')
-@stop
+  <title> Intégrate | Vázquez Hernández Contadores, S. C.
+  </title>
 
-@section('body')
-@stop
+  <link href="../../css/bootstrap.min.css" rel="stylesheet">
+  <link href="../../fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
 
-@section('content')
-    <div class="row">
-      <div class="col-md-12">
 
-        <div class="well transparenteClaroTxt animacionContenido">
+  <link href="../../css/publicos/pagina.css" rel="stylesheet">
+</head>
+  <body>
+
+    <?php include ("../../seccionesVista/barraNavegacionPagina.php"); ?>
+        <div class="fondoIntegrate fondo paddingMenu2">
+
           <div class="row">
-            <div class="col-md-12">
-              <div class="row text-center">
-                <h3 class="grisObscuro"><strong>Tu desarrollo profesional y personal</strong></h3>
+            <br>
+            <div class="col-md-3 col-md-offset-9">
+              <input type="text" name="name" value="" placeholder="Buscar en el sitio web">
+            </div>
+          </div>
+
+            <div class="row">
+              <div class="col-lg-12">
                 <br>
+                <h1 class="tituloEntrada tamTitulo grisObscuro"><i class="fa fa-puzzle-piece"></i> Intégrate</h1>
               </div>
+            </div>
 
-              <div class="row">
-                <div class="col-md-6 ">
-                  <img class="img-rounded img-responsive"  src="{{URL::asset('img/integrate/137830602.jpg')}}"/>
-                </div>
-                <div class="col-md-6">
-                  <p class="text-justify sangria textoNegro">
-                    En Vázquez Hernández Contadores, S.C., creemos que lo más importante es nuestro capital intelectual, ya que ello logra que la calidad de nuestra firma aumente, asumimos una visión clara de no integrarnos como empleados, sino como colaboradores de una gran familia que trabaja de manera ardua día con día para conseguir sus objetivos. Nuestra forma de trabajar conlleva ciertos valores como son: la confianza, compromiso, perseverancia, respeto, ética profesional, trabajo en equipo, excelencia, liderazgo y el entusiasmo; practicando estos valores tú serás una pieza clave que participará con nuestra firma en alcanzar objetivos mutuos.
-                  </p>
-                  <p class="text-justify textoNegro">
-                    Consolida el éxito de tu carrera profesional construyendo relaciones en una organización multidisciplinaria y reconocida en el mercado por enfocarse en agregar valor a sus clientes.
-                  </p>
-                </div>
-              </div>
-              <br>
-              <div class="row">
-                <div class="col-md-12">
-                  <p>
-                    Podrás relacionarte con profesionales con más de 20 años de experiencia en el área contable, fiscal y financiera; lo que generará en ti mejores experiencias al aplicar tu conocimiento, utilizando las mejores prácticas y soluciones ante circunstancias aparentemente complejas, logrando así mejores resultados.
-                  </p>
-                </div>
-                <div class="col-md-12">
-                  <p class="text-justify textoNegro">
-                    De esta manera tu trabajo resultará innovador, al combinar herramientas y experiencias que te permitan desempeñar tu profesión de manera por demás eficiente.
-                  </p>
-                </div>
-              </div>
+            <div class="row">
 
-              <div class="row">
-                <div class="col-md-12">
-                  <p class="text-justify textoNegro">
-                    Estamos convencidos de que el desarrollo tanto profesional como personal de todos y cada uno de los colaboradores que integran nuestra firma, contribuye al bienestar familiar, que es la base de una mejor sociedad.
-                  </p>
-                </div>
-              </div>
 
-            </div>  <!-- /col12 -->
-          </div> <!-- /renglon Profesionista-->
+              <div class="col-md-9"> <!-- Columna Contenido -->
+  <!--  -->      <!-- MUESTRA CONTENIDO ***************************************-->
+
+
+
+
+
+      <div class="row">
+        <div class="col-md-12">
+
+          <div class="well transparenteClaroTxt animacionContenido">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="row text-center">
+                  <h3 class="grisObscuro"><strong>Tu desarrollo profesional y personal</strong></h3>
+                  <br>
+                </div>
+
+                <div class="row">
+                  <div class="col-md-6 ">
+                    <img class="img-rounded img-responsive"  src="../../img/integrate/137830602.jpg"/>
+                  </div>
+                  <div class="col-md-6">
+                    <p class="text-justify sangria textoNegro">
+                      En Vázquez Hernández Contadores, S.C., creemos que lo más importante es nuestro capital intelectual, ya que ello logra que la calidad de nuestra firma aumente, asumimos una visión clara de no integrarnos como empleados, sino como colaboradores de una gran familia que trabaja de manera ardua día con día para conseguir sus objetivos. Nuestra forma de trabajar conlleva ciertos valores como son: la confianza, compromiso, perseverancia, respeto, ética profesional, trabajo en equipo, excelencia, liderazgo y el entusiasmo; practicando estos valores tú serás una pieza clave que participará con nuestra firma en alcanzar objetivos mutuos.
+                    </p>
+                    <p class="text-justify textoNegro">
+                      Consolida el éxito de tu carrera profesional construyendo relaciones en una organización multidisciplinaria y reconocida en el mercado por enfocarse en agregar valor a sus clientes.
+                    </p>
+                  </div>
+                </div>
+                <br>
+                <div class="row">
+                  <div class="col-md-12">
+                    <p>
+                      Podrás relacionarte con profesionales con más de 20 años de experiencia en el área contable, fiscal y financiera; lo que generará en ti mejores experiencias al aplicar tu conocimiento, utilizando las mejores prácticas y soluciones ante circunstancias aparentemente complejas, logrando así mejores resultados.
+                    </p>
+                  </div>
+                  <div class="col-md-12">
+                    <p class="text-justify textoNegro">
+                      De esta manera tu trabajo resultará innovador, al combinar herramientas y experiencias que te permitan desempeñar tu profesión de manera por demás eficiente.
+                    </p>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-md-12">
+                    <p class="text-justify textoNegro">
+                      Estamos convencidos de que el desarrollo tanto profesional como personal de todos y cada uno de los colaboradores que integran nuestra firma, contribuye al bienestar familiar, que es la base de una mejor sociedad.
+                    </p>
+                  </div>
+                </div>
+
+              </div>  <!-- /col12 -->
+            </div> <!-- /renglon Profesionista-->
+          </div>
+
+
+      </div>  <!-- /renglon Main -->
+  </div>  <!-- Well -->
+
+
+
+
+
+
+
+
+
+
+
+
+                <!-- ************************************************************************** -->
+              </div> <!-- /Columna Contenido -->
+
+              <div class="col-md-3 transladarIzquierda">  <!-- Columna Menu -->
+                        <!-- MENU DERECHO INTEGRATE DESDE menuIntegrate.blde.php -->
+                <?php include ("../../seccionesVista/menuIntegrate.php"); ?>
+
+              </div>  <!-- /Columna Menu -->
+
+
+          </div>
         </div>
 
 
-    </div>  <!-- /renglon Main -->
-</div>  <!-- Well -->
-@stop
+      <script src="../../js/jquery.js"></script>
+      <script src="../../js/bootstrap.min.js"></script>
+      <?php include ("../../seccionesVista/piePagina.php"); ?>
 
-@section('javascript')
-  <script charset="utf-8">
-    $('#liDesarrollo').addClass('activoBorde');
-  </script>
-@stop
+      <script charset="utf-8">
+        $('#liDesarrollo').addClass('activoBorde');
+      </script>
+
+      <script>
+        $('#navIntegrate').addClass('activoLi');
+        //jQuery to collapse the navbar on scroll
+        $(window).scroll(function() {
+            if ($(".navbar").offset().top > 50) {
+                $(".navbar-fixed-top").addClass("top-nav-collapse");
+            } else {
+                $(".navbar-fixed-top").removeClass("top-nav-collapse");
+            }
+        });
+      </script>
+
+  </body>
