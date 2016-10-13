2875b48caa134f59a74aab67c8b09ec80e0ba644
EdSM <edgar.santiago@contadoresvh.com>
2016-10-13 11:08:51 -0500

    articulo 7

--- vhc/publicos/infoteca/articulos/articulo7.php
+++ vhc/publicos/infoteca/articulos/articulo7.php
@@ -1,130 +1,181 @@
-@extends('layoutArticulo')
+<!DOCTYPE html>
+<html lang="es">
 
-@section('head')
-@stop
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <meta name="author" content="edSantiago" />
+  <link rel="icon" type="../../../image/png" href="./favicon.png"/>
 
-@section('title')
-  Artículos | Vázquez Hernández Contadores, S. C.
-@stop
+  <title>
 
-@section('css')
-@stop
+    Artículos | Vázquez Hernández Contadores, S. C.
+  </title>
 
-@section('body')
-@stop
+  <link href="../../../css/bootstrap.min.css" rel="stylesheet">
+  <link href="../../../fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
 
-@section('content')
-<div class="well transparenteClaroMTxt">  <!-- Well Articulo1 -->
-  <div class="row">
-    <div class="col-md-12">
+  <link href="../../../css/publicos/pagina.css" rel="stylesheet">
+</head>
 
-        <!-- CONTENIDO DEL ARTICULO -->
-        <h3>¿Has ejercido tus derechos ARCO?</h3>
-        <div class="row">
 
-          <div class="col-md-4">
-            <br>
-            <img class="img-rounded img-responsive imgN" src="{{URL::asset('img/infoteca/articulos/135520668.jpg')}}"/>
-          </div>
+  <body>
+      <?php include ("../../../seccionesVista/barraNavegacionPagina.php"); ?>
 
-          <div class="col-md-8">
-            <p class="text-justify">
-              Cuantas veces no te han llamado para ofrecerte una tarjeta de crédito, el cambio de tu número de celular a otra compañía, o una invitación para un evento de una agencia de viajes, cuando tú no lo has solicitado y mucho menos recuerdas haber compartido tus datos con estas empresas, ¿te suena familiar?
-            </p>
-
-            <p class="text-justify">
-              Pues así es, muchas personas en el país sufren de lo mismo, y esto es debido a que las empresas están haciendo mal uso de nuestros datos personales, ya que comparten o venden a otras compañías sus bases de datos. ¿Qué podemos hacer ante tal situación? Lo primero es proteger nuestros datos personales, no compartiéndolos con cualquier persona u organización, y si es necesario compartirlos, entonces asegurarnos de que esa persona o empresa cuenta con su Aviso de Privacidad, pero no sólo eso, es indispensable dar lectura a dicho documento para conocer las finalidades para las cuales utilizarán nuestros datos personales.
-            </p>
-
+        <div class="fondoInfoteca fondo paddingMenu">
+            <div class="row">
+              <div class="col-lg-12">
+                <br>
+                <h1 class="tamTitulo grisClaro texto3D tituloEntrada"><i class="fa fa-book"></i> Artículos</h1>
+                <br>
+              </div>
             </div>
 
+            <div class="row">
+
+              <div class="col-md-9 animacionContenido">
+                 <!--CONTENIDO DEL ARTICULO **************************-->
+
+                 <div class="well transparenteClaroMTxt">  <!-- Well Articulo1 -->
+     <div class="row">
+       <div class="col-md-12">
+
+           <!-- CONTENIDO DEL ARTICULO -->
+           <h3>¿Has ejercido tus derechos ARCO?</h3>
+           <div class="row">
+
+             <div class="col-md-4">
+               <br>
+               <img class="img-rounded img-responsive imgN" src="../../../img/infoteca/articulos/135520668.jpg"/>
+             </div>
+
+             <div class="col-md-8">
+               <p class="text-justify">
+                 Cuantas veces no te han llamado para ofrecerte una tarjeta de crédito, el cambio de tu número de celular a otra compañía, o una invitación para un evento de una agencia de viajes, cuando tú no lo has solicitado y mucho menos recuerdas haber compartido tus datos con estas empresas, ¿te suena familiar?
+               </p>
+
+               <p class="text-justify">
+                 Pues así es, muchas personas en el país sufren de lo mismo, y esto es debido a que las empresas están haciendo mal uso de nuestros datos personales, ya que comparten o venden a otras compañías sus bases de datos. ¿Qué podemos hacer ante tal situación? Lo primero es proteger nuestros datos personales, no compartiéndolos con cualquier persona u organización, y si es necesario compartirlos, entonces asegurarnos de que esa persona o empresa cuenta con su Aviso de Privacidad, pero no sólo eso, es indispensable dar lectura a dicho documento para conocer las finalidades para las cuales utilizarán nuestros datos personales.
+               </p>
+
+               </div>
+
+           </div>
+
+
+                       <p class="text-justify">
+                         Recordemos que el Aviso de Privacidad es el documento mediante el cual la persona o empresa con la cual compartes tus datos personales, ya sea porque te prestará un servicio, por una relación laboral, por la venta de algún bien, etc., te informa respecto al tratamiento que dará a los mismos, es decir si sólo los utilizará para emitirte tu factura, porque son necesarios para prestarte el servicio, con motivo de una encuesta, para efectos estadísticos o para promoción comercial, etc. El aviso de privacidad te da el control de tus datos personales y te da seguridad respecto a con quién los compartes.
+                       </p>
+
+
+           <p class="text-justify">
+             Lo segundo que podemos hacer cuando creemos que nuestros datos personales han sido vulnerados, es “Ejercitar nuestros derechos ARCO”, y no me refiero a alguna rutina de ejercicios, me refiero a lo siguiente:
+           </p>
+
+           <h4>¿Qué son los derechos ARCO?</h4>
+           <p class="text-justify">
+             Son los derechos que pueden ejercer los individuos para proteger sus datos personales. La palabra ARCO se refiere a <b>A</b>cceder, <b>R</b>ectificar, <b>C</b>ancelar y <b>O</b>ponerse al tratamiento de sus datos personales.
+           </p>
+
+           <p class="text-justify">
+             El derecho de <strong>Acceso</strong>, es la puerta de entrada para que conozcas cuáles de tus datos personales están en poder de cierta entidad o persona, y de esta forma estés en posibilidades de ejercer los demás derechos.
+           </p>
+
+           <p class="text-justify">
+             El derecho de <strong>Rectificación</strong>, está enfocado a controlar la calidad de la información, esto es si existen errores en tus datos personales o es necesario actualizarlos, entonces es momento de ejercer esta facultad.
+           </p>
+           <p class="text-justify">
+             El derecho de <strong>Cancelación</strong> implica la solicitud de eliminación de tus datos personales porque éstos están siendo tratados indebidamente.
+           </p>
+           <p class="text-justify">
+             El derecho de <strong>Oposición</strong> se puede ejercer cuando no queremos que nuestros datos personales sean utilizados para ciertos fines, es decir, daremos autorización para que nuestra información sea utilizada para ciertas actividades y para otras no.
+           </p>
+
+           <ul>
+             <li>
+               Cuando deseamos conocer los datos personales que tiene registrados cierta institución;
+             </li>
+             <li>
+               Cuando es preciso actualizarlos debido a que ciertos datos personales son incorrectos, por ejemplo: el domicilio que tiene registrado el banco para la entrega de nuestros estados de cuenta, o el número telefónico;
+             </li>
+             <li>
+               Cuando creemos que nuestros datos personales han sido vulnerados y por tal motivo deseamos que los supriman de ciertas bases de datos, por ejemplo, cuando recibes llamadas de una compañía de servicio de telefonía celular diferente a la que te presta el servicio, cuando recibes llamadas del banco para ofrecerte nuevos servicios, etcétera, y la característica principal, es que son empresas de las que nunca en la vida habías escuchado y mucho menos recuerdas haberles proporcionado tus datos, inclusive cuando contrataste algún tipo de servicio por única ocasión y que posteriormente continúan constantemente llamando para ofrecerte nuevos productos o servicios;
+             </li>
+             <li>
+               Cuando nuestros datos personales están siendo utilizados para otros fines, diferentes a lo que inicialmente autorizaste. Por ejemplo, cuando contratamos el servicio de hospedaje, nuestros datos son indispensable para que a la empresa le sea posible prestarnos el servicio, pero sólo para eso, no obstante la empresa hotelera utiliza tus datos para llamar constantemente y ofrecerte descuentos y promociones, cuando tú no los has solicitado.
+
+             </li>
+           </ul>
+
+           <h4>¿Cómo puedes ejercitar tus derechos ARCO?
+           </h4>
+           <p class="text-justify">
+             Muy fácil, sólo tienes que dar lectura al Aviso de Privacidad de la empresa o persona que está utilizando tus datos personales y verificar cuál es el procedimiento y los requisitos para ejercer tus derechos ARCO. Lo anterior puede ser mediante el envío, vía correo electrónico o certificado, de una solicitud al Encargado del Departamento de Datos Personales de la empresa en cuestión o acudiendo personalmente a ejercer tus derechos. Dicha solicitud deberá contener por lo menos los siguientes datos: tu nombre, domicilio o <i>email</i> para comunicarte la respuesta, copia de alguna identificación, y una descripción clara y precisa de los datos personales respecto de los cuales deseas ejercer alguno de los derechos ARCO.
+           </p>
+           <br>
+
+           <p  class="text-justify">
+               El responsable de la entidad del manejo de tus datos personales deberá dar respuesta a tu petición a más tardar en 20 días hábiles, siempre y cuando hayas enviado en su totalidad los datos requeridos en la solicitud.
+           </p>
+
+           <p>
+             <strong>
+               No en todos los casos es posible ejercer el derecho de cancelación y oposición, esto es cuando exista alguna resolución de una autoridad competente, cuando existe algún impedimento legal, o si tus datos personales son indispensable para cumplir con obligaciones legales.
+             </strong>
+           </p>
+
+           <p>
+             Es importante destacar que los trámites para ejercitar tus derechos ARCO son estrictamente personales, por lo que no podrá ejercerlos otra persona que no sea tu, a menos que tengas un representante legal.
+           </p>
+
+         <!-- /CONTENIDO DEL ARTICULO -->
+
+       </div>
+     </div>
+     <small>Autora: L.C. Bibiana I. Nuñez Aguilar</small>
+     <br>
+     <small>
+     1. Albacea.- Persona encargada por el testador o por el juez de cumplir la última voluntad del finado, custodiando sus bienes y dándoles el destino que corresponde según la herencia. Diccionario de la Real Academia Española.
+     </small>
+   </div>  <!-- /Well Articulo1 -->
+
+
+
+
+
+
+                 <!-- ************************************************************************************ -->
+              </div>
+
+              <div class="col-md-3 transladarIzquierda">
+                  <!--MENU DE LOS ARTICULOS menuArticulo.blde.php -->
+                <?php include ("../../../seccionesVista/menuArticulo.php"); ?>
+                <br>
+                <a class="btn btn-primary btn-block" href="javascript:history.back()"><i class="fa fa-chevron-left"></i><span class="network-name"> Regresar Infoteca</span></a>
+              </div>
+
+          </div>
         </div>
 
+        <?php include ("../../../seccionesVista/piePagina.php"); ?>
+        <script src="../../../js/jquery.js"></script>
+        <script src="../../../js/bootstrap.min.js"></script>
+      <script>
+        $('#navInfoteca').addClass('activoLi');
+        //jQuery to collapse the navbar on scroll
+        $(window).scroll(function() {
+            if ($(".navbar").offset().top > 50) {
+                $(".navbar-fixed-top").addClass("top-nav-collapse");
+            } else {
+                $(".navbar-fixed-top").removeClass("top-nav-collapse");
+            }
+        });
+      </script>
 
-                    <p class="text-justify">
-                      Recordemos que el Aviso de Privacidad es el documento mediante el cual la persona o empresa con la cual compartes tus datos personales, ya sea porque te prestará un servicio, por una relación laboral, por la venta de algún bien, etc., te informa respecto al tratamiento que dará a los mismos, es decir si sólo los utilizará para emitirte tu factura, porque son necesarios para prestarte el servicio, con motivo de una encuesta, para efectos estadísticos o para promoción comercial, etc. El aviso de privacidad te da el control de tus datos personales y te da seguridad respecto a con quién los compartes.
-                    </p>
-
-
-        <p class="text-justify">
-          Lo segundo que podemos hacer cuando creemos que nuestros datos personales han sido vulnerados, es “Ejercitar nuestros derechos ARCO”, y no me refiero a alguna rutina de ejercicios, me refiero a lo siguiente:
-        </p>
-
-        <h4>¿Qué son los derechos ARCO?</h4>
-        <p class="text-justify">
-          Son los derechos que pueden ejercer los individuos para proteger sus datos personales. La palabra ARCO se refiere a <b>A</b>cceder, <b>R</b>ectificar, <b>C</b>ancelar y <b>O</b>ponerse al tratamiento de sus datos personales.
-        </p>
-
-        <p class="text-justify">
-          El derecho de <strong>Acceso</strong>, es la puerta de entrada para que conozcas cuáles de tus datos personales están en poder de cierta entidad o persona, y de esta forma estés en posibilidades de ejercer los demás derechos.
-        </p>
-
-        <p class="text-justify">
-          El derecho de <strong>Rectificación</strong>, está enfocado a controlar la calidad de la información, esto es si existen errores en tus datos personales o es necesario actualizarlos, entonces es momento de ejercer esta facultad.
-        </p>
-        <p class="text-justify">
-          El derecho de <strong>Cancelación</strong> implica la solicitud de eliminación de tus datos personales porque éstos están siendo tratados indebidamente.
-        </p>
-        <p class="text-justify">
-          El derecho de <strong>Oposición</strong> se puede ejercer cuando no queremos que nuestros datos personales sean utilizados para ciertos fines, es decir, daremos autorización para que nuestra información sea utilizada para ciertas actividades y para otras no.
-        </p>
-
-        <ul>
-          <li>
-            Cuando deseamos conocer los datos personales que tiene registrados cierta institución;
-          </li>
-          <li>
-            Cuando es preciso actualizarlos debido a que ciertos datos personales son incorrectos, por ejemplo: el domicilio que tiene registrado el banco para la entrega de nuestros estados de cuenta, o el número telefónico;
-          </li>
-          <li>
-            Cuando creemos que nuestros datos personales han sido vulnerados y por tal motivo deseamos que los supriman de ciertas bases de datos, por ejemplo, cuando recibes llamadas de una compañía de servicio de telefonía celular diferente a la que te presta el servicio, cuando recibes llamadas del banco para ofrecerte nuevos servicios, etcétera, y la característica principal, es que son empresas de las que nunca en la vida habías escuchado y mucho menos recuerdas haberles proporcionado tus datos, inclusive cuando contrataste algún tipo de servicio por única ocasión y que posteriormente continúan constantemente llamando para ofrecerte nuevos productos o servicios;
-          </li>
-          <li>
-            Cuando nuestros datos personales están siendo utilizados para otros fines, diferentes a lo que inicialmente autorizaste. Por ejemplo, cuando contratamos el servicio de hospedaje, nuestros datos son indispensable para que a la empresa le sea posible prestarnos el servicio, pero sólo para eso, no obstante la empresa hotelera utiliza tus datos para llamar constantemente y ofrecerte descuentos y promociones, cuando tú no los has solicitado.
-
-          </li>
-        </ul>
-
-        <h4>¿Cómo puedes ejercitar tus derechos ARCO?
-        </h4>
-        <p class="text-justify">
-          Muy fácil, sólo tienes que dar lectura al Aviso de Privacidad de la empresa o persona que está utilizando tus datos personales y verificar cuál es el procedimiento y los requisitos para ejercer tus derechos ARCO. Lo anterior puede ser mediante el envío, vía correo electrónico o certificado, de una solicitud al Encargado del Departamento de Datos Personales de la empresa en cuestión o acudiendo personalmente a ejercer tus derechos. Dicha solicitud deberá contener por lo menos los siguientes datos: tu nombre, domicilio o <i>email</i> para comunicarte la respuesta, copia de alguna identificación, y una descripción clara y precisa de los datos personales respecto de los cuales deseas ejercer alguno de los derechos ARCO.
-        </p>
-        <br>
-
-        <p  class="text-justify">
-            El responsable de la entidad del manejo de tus datos personales deberá dar respuesta a tu petición a más tardar en 20 días hábiles, siempre y cuando hayas enviado en su totalidad los datos requeridos en la solicitud.
-        </p>
-
-        <p>
-          <strong>
-            No en todos los casos es posible ejercer el derecho de cancelación y oposición, esto es cuando exista alguna resolución de una autoridad competente, cuando existe algún impedimento legal, o si tus datos personales son indispensable para cumplir con obligaciones legales.
-          </strong>
-        </p>
-
-        <p>
-          Es importante destacar que los trámites para ejercitar tus derechos ARCO son estrictamente personales, por lo que no podrá ejercerlos otra persona que no sea tu, a menos que tengas un representante legal.
-        </p>
-
-      <!-- /CONTENIDO DEL ARTICULO -->
-
-    </div>
-  </div>
-  <small>Autora: L.C. Bibiana I. Nuñez Aguilar</small>
-  <br>
-  <small>
-  1. Albacea.- Persona encargada por el testador o por el juez de cumplir la última voluntad del finado, custodiando sus bienes y dándoles el destino que corresponde según la herencia. Diccionario de la Real Academia Española.
-  </small>
-</div>  <!-- /Well Articulo1 -->
-
-@stop
-
-@section('js')
-@stop
-
-@section('javascript')
-<script>
-  $('#ulArtTem1').addClass('in');
-  $('#liArt7').addClass('activoBorde');
-</script>
-@stop
+      <script>
+    $('#ulArtTem1').addClass('in');
+    $('#liArt7').addClass('activoBorde');
+  </script>
+  </body>
+</html>
