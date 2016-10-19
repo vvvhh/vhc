24a33734082b36e91d69dfca573eb1b05785d19c
EdSM <edgar.santiago@contadoresvh.com>
2016-10-19 18:14:43 -0500

    articulo 4

--- vhc/publicos/infoteca/articulos/articulo4.php
+++ vhc/publicos/infoteca/articulos/articulo4.php
@@ -1,19 +1,43 @@
-@extends('layoutArticulo')
+<!DOCTYPE html>
+<html lang="es">
 
-@section('head')
-@stop
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <meta name="author" content="edSantiago" />
+  <link rel="icon" type="../../../image/png" href="./favicon.png"/>
 
-@section('title')
-  Artículos | Vázquez Hernández Contadores, S. C.
-@stop
+  <title>
 
-@section('css')
-@stop
+    Artículos | Vázquez Hernández Contadores, S. C.
+  </title>
 
-@section('body')
-@stop
+  <link href="../../../css/bootstrap.min.css" rel="stylesheet">
+  <link href="../../../fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
 
-@section('content')
+  <link href="../../../css/publicos/pagina.css" rel="stylesheet">
+</head>
+
+
+  <body>
+      <?php include ("../../../seccionesVista/barraNavegacionPagina.php"); ?>
+
+        <div class="fondoInfoteca fondo paddingMenu">
+            <div class="row">
+              <div class="col-lg-12">
+                <br>
+                <h1 class="tamTitulo grisClaro texto3D tituloEntrada"><i class="fa fa-book"></i> Artículos</h1>
+                <br>
+              </div>
+            </div>
+
+            <div class="row">
+
+              <div class="col-md-9 animacionContenido">
+                 <!--CONTENIDO DEL ARTICULO **************************-->
+
+
 <div class="well transparenteClaroMTxt">  <!-- Well Articulo1 -->
   <div class="row">
     <div class="col-md-12">
@@ -46,7 +70,7 @@
             </div>
             <div class="col-md-4">
               <br>
-              <img class="img-rounded img-responsive imgN" src="{{URL::asset('img/infoteca/resenas/87158796.jpg')}}"/>
+              <img class="img-rounded img-responsive imgN" src="../../../img/infoteca/resenas/87158796.jpg"/>
             </div>
         </div>
 
@@ -123,14 +147,38 @@
   <small>Autora: L.C. Bibiana I. Nuñez Aguilar</small>
 </div>  <!-- /Well Articulo1 -->
 
-@stop
+<!-- ************************************************************************************ -->
+</div>
 
-@section('js')
-@stop
+<div class="col-md-3 transladarIzquierda">
+   <!--MENU DE LOS ARTICULOS menuArticulo.blde.php -->
+  <?php include ("../../../seccionesVista/menuArticulo.php"); ?>
+  <br>
+  <a class="btn btn-primary btn-block" href="javascript:history.back()"><i class="fa fa-chevron-left"></i><span class="network-name"> Regresar Infoteca</span></a>
+</div>
 
-@section('javascript')
+</div>
+</div>
+
+<?php include ("../../../seccionesVista/piePagina.php"); ?>
+<script src="../../../js/jquery.js"></script>
+<script src="../../../js/bootstrap.min.js"></script>
 <script>
-  $('#ulArtTem1').addClass('in');
-  $('#liArt4').addClass('activoBorde');
+$('#navInfoteca').addClass('activoLi');
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+  if ($(".navbar").offset().top > 50) {
+    $(".navbar-fixed-top").addClass("top-nav-collapse");
+  } else {
+    $(".navbar-fixed-top").removeClass("top-nav-collapse");
+  }
+});
 </script>
-@stop
+
+<script>
+$('#ulArtTem1').addClass('in');
+$('#liArt4').addClass('activoBorde');
+</script>
+
+</body>
+</html>
