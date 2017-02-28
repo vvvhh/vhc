d501897f184b0411c889537d6a0033f5393d87ce
EdSM <edgar.santiago@contadoresvh.com>
2017-02-28 11:17:56 -0600

    contactenos cambio foumulario

--- publicos/contacto.php
+++ publicos/contacto.php
@@ -39,7 +39,7 @@
 
     <?php include ("../seccionesVista/barraNavegacionPagina.php"); ?>
 
-<div style="background-image:url('../img/contacto/491522385.jpg ');"class="fondo paddingTexto">
+<div style="background-image:url('../img/contacto/491522385.jpg ');"class="fondo">
 
   <div class="row">
     <br>
@@ -58,94 +58,6 @@
     </div>
   </div>
 
-
-
-  <div class="row">
-
-    <div class="col-md-12" >
-      <div class="well transparenteObscuro transladarAbajo" id="mapaDF"><!-- Map-->
-        <div class="row">
-          <h3 class="text-center texto3D grisClaro">Ciudad de México</h3>
-          <hr>
-          <div class="col-md-8 transladarDerecha">
-            <iframe width="100%" height="400px" frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3763.1215612285746!2d-99.1592!3d19.407153000000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff1635487267%3A0xd84dc57705c9e4c4!2sV%C3%A1zquez+Hern%C3%A1ndez+Contadores%2C+S.C.!5e0!3m2!1ses!2smx!4v1426613320449" ></iframe>
-          </div>
-          <div class="col-md-4 transladarIzquierda">
-            <br><br>
-            <p class="texto3D grisClaro">
-              <i class="fa fa-map-marker"></i> Huatabampo 72 Int. 1, 2<br> Col. Roma Sur, Cuauhtémoc, CP.06760<br>Cd. México, México<br>
-            </p>
-            <p class="texto3D grisClaro">
-              <i class="fa fa-phone"></i> 55 74 41 91 - 55 74 27 95 <br> 55 64 38 07 - 55 84 26 12 </p>
-            <p class="texto3D grisClaro">
-
-            <p class="texto3D grisClaro">
-                <i class="fa fa-clock-o"></i> Lunes - Viernes: 8:00 AM a 6:00 PM</p>
-            <p class="texto3D grisClaro">
-                <i class="fa fa-whatsapp" aria-hidden="true"></i> 55 55 08 52 13
-            </p>
-            <p class="texto3D grisClaro">
-              <i class="fa fa-envelope" aria-hidden="true"></i>
-              contacto.cdmx@contadoresvh.com
-            </p>
-          </div>
-        </div>
-
-      </div>
-    </div>
-
-  </div>
-
-
-  <div class="row">
-    <div class="col-md-6">
-      <div class="well transparenteObscuro transladarDerecha" id="mapaCancun">
-        <h3 class="text-center texto3D grisClaro">Cancún</h3><hr>
-        <div class="row">
-          <div class="col-md-8">
-            <iframe width="100%" height="200px" frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d1860.591693979294!2d-86.83033263819414!3d21.145098919750577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1s+Av.+Xpuhil+sur+!5e0!3m2!1sen!2smx!4v1470782484505"></iframe>
-            <!--    <iframe width="100%" height="200px" frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d14884.960312646033!2d-86.829949!3d21.142842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sPlaza+Atrium+CP.+77500%2C+Canc%C3%BAn%2C+Quintana+Roo.!5e0!3m2!1ses-419!2smx!4v1425600726657"></iframe> -->
-          </div>
-          <div class="col-md-4">
-            <p class="texto3D grisClaro">
-              <i class="fa fa-map-marker"></i>  Av. Xpuhil sur <br>Mza. 2, Lote 23,<br> Despacho 201, 208 <br> Supermanzana 19, <br> Benito Juárez, <br>C.P. 77505 <br> Quintana Roo. <br>
-              <p class="texto3D grisClaro"><i class="fa fa-phone"></i> (998) 868 30 55</p>
-            </p>
-
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12 text-center texto3D grisClaro">
-            <i class="fa fa-envelope" aria-hidden="true"></i>
-            contacto.cancun@contadoresvh.com
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-6">
-      <div class="well transparenteObscuro transladarIzquierda" id="mapaCancun">
-        <h3 class="text-center texto3D grisClaro">Mérida</h3>   <hr>
-        <div class="row">
-          <div class="col-md-8">
-            <iframe  width="100%" height="200" frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3724.4691890303093!2d-89.65561181162407!3d21.013904579551106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2smx!4v1425748093767"></iframe>
-          </div>
-          <div class="col-md-4">
-            <p class="texto3D grisClaro">
-              <i class="fa fa-map-marker"></i>  Calle 33 No. 618A<br> Col. Pedregales de Ciudad Caucel, CP. 97314,<br> Mérida, Yucatán.<br>
-            </p>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12 text-center texto3D grisClaro">
-            <i class="fa fa-envelope" aria-hidden="true"></i>
-            contacto.merida@contadoresvh.com
-          </div>
-        </div>
-      </div>
-    </div>
-
-  </div>
 
   <div class="row">
           <!-- Formulario Mensaje -->
@@ -210,8 +122,155 @@
             </div>
 
           </div>
+
+<br>
+
+      <!--      <div class="col-md-12" >
+            <div class="well transparenteObscuro transladarAbajo" id="mapaDF">
+              <div class="row">
+                <h3 class="text-center texto3D grisClaro">Ciudad de México</h3>
+                <hr>
+                <div class="col-md-8 transladarDerecha">
+                  <iframe width="100%" height="400px" frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3763.1215612285746!2d-99.1592!3d19.407153000000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff1635487267%3A0xd84dc57705c9e4c4!2sV%C3%A1zquez+Hern%C3%A1ndez+Contadores%2C+S.C.!5e0!3m2!1ses!2smx!4v1426613320449" ></iframe>
+                </div>
+                <div class="col-md-4 transladarIzquierda">
+                  <br><br>
+                  <p class="texto3D grisClaro">
+                    <i class="fa fa-map-marker"></i> Huatabampo 72 Int. 1, 2<br> Col. Roma Sur, Cuauhtémoc, CP.06760<br>Cd. México, México<br>
+                  </p>
+                  <p class="texto3D grisClaro">
+                    <i class="fa fa-phone"></i> 55 74 41 91 - 55 74 27 95 <br> 55 64 38 07 - 55 84 26 12 </p>
+                  <p class="texto3D grisClaro">
+
+                  <p class="texto3D grisClaro">
+                      <i class="fa fa-clock-o"></i> Lunes - Viernes: 8:00 AM a 6:00 PM</p>
+                  <p class="texto3D grisClaro">
+                      <i class="fa fa-whatsapp" aria-hidden="true"></i> 55 55 08 52 13
+                  </p>
+                  <p class="texto3D grisClaro">
+                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                    contacto.cdmx@contadoresvh.com
+                  </p>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+-->
+
+
+
+
           <!--  ./Formulario Mensaje -->
         </div>
+
+
+
+  <div class="row">
+
+
+
+  </div>
+
+
+  <div class="row">
+
+    <div class="col-md-6" >
+          <div class="well transparenteObscuro transladarAbajo" id="mapaDF">
+            <div class="row">
+              <h3 class="text-center texto3D grisClaro">Ciudad de México</h3>
+              <hr>
+              <div class="col-md-8 transladarDerecha">
+                <iframe width="100%" height="300px" frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3763.1215612285746!2d-99.1592!3d19.407153000000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff1635487267%3A0xd84dc57705c9e4c4!2sV%C3%A1zquez+Hern%C3%A1ndez+Contadores%2C+S.C.!5e0!3m2!1ses!2smx!4v1426613320449" ></iframe>
+              </div>
+              <div class="col-md-4 transladarIzquierda">
+                <br><br>
+                <p class="texto3D grisClaro">
+                  <i class="fa fa-map-marker"></i> Huatabampo 72 <br> Int. 1, 2<br> Col. Roma Sur, Cuauhtémoc, CP.06760<br>Cd. México, México<br>
+                </p>
+                <p class="texto3D grisClaro">
+                  <i class="fa fa-phone"></i> 55 74 41 91 <br> 55 74 27 95 <br> 55 64 38 07 <br> 55 84 26 12 </p>
+                <p class="texto3D grisClaro">
+
+                <p class="texto3D grisClaro">
+                    <i class="fa fa-clock-o"></i> Lunes - Viernes: 8:00 AM a 6:00 PM</p>
+              <!--    <p class="texto3D grisClaro">
+                    <i class="fa fa-whatsapp" aria-hidden="true"></i> 55 55 08 52 13
+                </p>
+-->
+              </div>
+            </div>
+            <br>
+
+            <div class="row">
+              <div class="col-md-12 texto3D grisClaro text-center">
+                  <i class="fa fa-envelope" aria-hidden="true"></i>
+                  contacto.cdmx@contadoresvh.com
+
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+
+    <div class="col-md-6">
+      <div class="well transparenteObscuro transladarDerecha" id="mapaCancun">
+        <h3 class="text-center texto3D grisClaro">Cancún</h3><hr>
+        <div class="row">
+          <div class="col-md-8">
+            <iframe width="100%" height="300px" frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d1860.591693979294!2d-86.83033263819414!3d21.145098919750577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1s+Av.+Xpuhil+sur+!5e0!3m2!1sen!2smx!4v1470782484505"></iframe>
+            <!--    <iframe width="100%" height="200px" frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d14884.960312646033!2d-86.829949!3d21.142842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sPlaza+Atrium+CP.+77500%2C+Canc%C3%BAn%2C+Quintana+Roo.!5e0!3m2!1ses-419!2smx!4v1425600726657"></iframe> -->
+          </div>
+          <div class="col-md-4">
+            <br><br>
+            <p class="texto3D grisClaro">
+              <i class="fa fa-map-marker"></i>  Av. Xpuhil sur <br>Mza. 2, Lote 23,<br> Despacho 201, 208 <br> Supermanzana 19, <br> Benito Juárez, <br>C.P. 77505 <br> Quintana Roo, Méx. <br>
+              <p class="texto3D grisClaro"><i class="fa fa-phone"></i> (998) 868 30 55</p>
+            </p>
+
+          </div>
+        </div>
+        <br>
+        <div class="row">
+          <div class="col-md-12 text-center texto3D grisClaro">
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+            contacto.cancun@contadoresvh.com
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+
+  <!--    <div class="col-md-6">
+      <div class="well transparenteObscuro transladarIzquierda" id="mapaCancun">
+        <h3 class="text-center texto3D grisClaro">Mérida</h3>   <hr>
+        <div class="row">
+          <div class="col-md-8">
+            <iframe  width="100%" height="200" frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3724.4691890303093!2d-89.65561181162407!3d21.013904579551106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2smx!4v1425748093767"></iframe>
+          </div>
+          <div class="col-md-4">
+            <p class="texto3D grisClaro">
+              <i class="fa fa-map-marker"></i>  Calle 33 No. 618A<br> Col. Pedregales de Ciudad Caucel, CP. 97314,<br> Mérida, Yucatán.<br>
+            </p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center texto3D grisClaro">
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+            contacto.merida@contadoresvh.com
+          </div>
+        </div>
+      </div>
+    </div>
+    -->
+
+  </div>
+
+<br><br><br> 
 
   </div>  <!-- /container -->
 
